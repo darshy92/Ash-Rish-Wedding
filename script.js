6cab27077d6c6e20f00bbf38bb34974e05b94607
darshy92 <154074228+darshy92@users.noreply.github.com>
2023-12-18 01:27:12 +0800

    Update script.js
    changed the matching

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function findSeat() {
-    var name = document.getElementById('name').value;
+    var name = document.getElementById('name').value.toLowerCase();
 
     // Check if the name exists in the seating data
-    var seatInfo = seatingData.find(item => item.name.toLowerCase() === name.toLowerCase());
+    var matchingSeats = seatingData.filter(item => {
+        return item.name.toLowerCase().includes(name);
+    });
 
-    if (seatInfo) {
-        var tableNumber = seatInfo.tableNumber;
-        document.getElementById('result').innerHTML = name + ', your seat is at Table ' + tableNumber;
+    if (matchingSeats.length > 0) {
+        var resultHtml = '<p>Multiple matches found:</p>';
+        matchingSeats.forEach(seat => {
+            resultHtml += `<p>${seat.name}, your seat is at Table ${seat.tableNumber}</p>`;
+        });
+        document.getElementById('result').innerHTML = resultHtml;
     } else {
-        document.getElementById('result').innerHTML = 'Name not found. Please check your entry.';
+        document.getElementById('result').innerHTML = 'Name not found. Please check your spelling and ensure there are no spaces after your name.';
     }
 }
